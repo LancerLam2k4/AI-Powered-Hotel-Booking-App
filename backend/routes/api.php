7052f96f5a893348e0ee30969f250e048811b928
LancerLam2k4 <141577920+LancerLam2k4@users.noreply.github.com>
2024-10-13 21:44:32 +0700

    Configure email and authenticate registration with email

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,3 +3,6 @@ use App\Http\Controllers\AuthController;
 
 Route::post('register', [AuthController::class, 'registerTraveler']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('/check-email', [AuthController::class, 'checkEmailAndSendCode']);
+Route::post('/verify-code', [AuthController::class, 'verifyCode']);
+
