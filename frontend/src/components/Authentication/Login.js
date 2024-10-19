6413c182806146e6144c1b4343111b623f7497fa
NguyenNamTruong <nguyennamrruong2014@gmail.com>
2024-10-19 23:38:09 +0700

    Frontend forgotpassword.css

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 import ForgotPassword from './ForgotPassword';
-
+import './ForgotPassword.css';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
