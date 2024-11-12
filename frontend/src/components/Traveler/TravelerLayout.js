db9b250e4e5aed12475f111f8ac8ce6ef3e97676
NguyenNamTruong <nguyennamrruong2014@gmail.com>
2024-11-12 23:20:07 +0700

    updateNavbar

--- frontend/src/components/Traveler/TravelerLayout.js
+++ frontend/src/components/Traveler/TravelerLayout.js
@@ -7,6 +7,25 @@ import './TravelerLayout.css';
 const TravelerLayout = () => {
   return (
     <div className="traveler-layout">
+       <header className="header-container">
+      <div className="header-container">
+      <div className="logo-container">
+        <img src="logo3.png" alt="Logo1" className="logo1" />
+      </div>
+
+      <div className="nav-links">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Services</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </div>
+      <button className="booking-button">
+      <span className="material-icons">shopping_cart</span> 
+      </button>
+    </div>
+    </header>
       {/* Khung chứa các trang con của Traveler */}
       <div className="traveler-content">
         <Outlet /> {/* Render nội dung của các trang con (Profile, Booking...) */}
